4916e96e220687c87f4e0bed66bbc3c88031c298
Helen Cantu <helen_beatriz02@outlook.com>
2023-03-12 19:16:10 +0000

    add comment form

--- djangorecipes/settings.py
+++ djangorecipes/settings.py
@@ -51,13 +51,16 @@ INSTALLED_APPS = [
     'cloudinary',
     'django_summernote',
     'djangorecipes',
-    'blog'
+    'crispy_forms',
+    'blog',
 ]
 
 SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
