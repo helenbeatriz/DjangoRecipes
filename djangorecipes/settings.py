d64c2c871e49f9af150a231187601ea8a59a6c52
Helen Cantu <helen_beatriz02@outlook.com>
2023-03-12 14:04:32 +0000

    fix bug on live website

--- djangorecipes/settings.py
+++ djangorecipes/settings.py
@@ -80,6 +80,25 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'djangorecipes.wsgi.application'
 
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
+SUMMERNOTE_THEME = 'bs4'
+
+SUMMERNOTE_CONFIG = {
+    'iframe': False,
+    'toolbar': [
+        ['style', ['style']],
+        ['font', ['bold', 'italic', 'underline', 'strikethrough', 'clear']],
+        ['fontname', ['fontname']],
+        ['color', ['color']],
+        ['para', ['ul', 'ol', 'paragraph']],
+        ['table', ['table']],
+        ['insert', ['link', 'picture', 'video']],
+        ['view', ['fullscreen', 'codeview', 'help']],
+    ],
+    'width': '100%',
+    'height': '480',
+}
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
