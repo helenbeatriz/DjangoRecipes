643dc00cfc5583a1bd78f0fe14b129b153c14211
Helen Cantu <helen_beatriz02@outlook.com>
2023-03-12 15:18:59 +0000

    add django login features and customization

--- djangorecipes/settings.py
+++ djangorecipes/settings.py
@@ -42,6 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
@@ -49,6 +53,11 @@ INSTALLED_APPS = [
     'djangorecipes',
     'blog'
 ]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -61,6 +70,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'djangorecipes.urls'
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 TEMPLATES = [
     {
