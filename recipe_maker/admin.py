84ad773f0914850a9641443d4c841e5d600f2f70
Helen Cantu <helen_beatriz02@outlook.com>
2023-03-28 12:50:58 +0000

    Created recipe maker model

--- recipe_maker/admin.py
+++ recipe_maker/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Recipe
 
-# Register your models here.
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ("title", "description", "meal_type", "cuisine_types", "calories", "posted_date")
+
+    list_filter = ("meal_type",)
