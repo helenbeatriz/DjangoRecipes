0612573e4a1adf70c74f3a94badb41e31eabb089
Helen Cantu <helen_beatriz02@outlook.com>
2023-04-13 17:22:39 +0000

    Add styling and content for recipes

--- recipe_maker/urls.py
+++ recipe_maker/urls.py
@@ -6,6 +6,6 @@ app_name = "recipe_maker"
 urlpatterns = [
     path("add_recipe/", AddRecipe.as_view(), name="add_recipe"),
     path("recipes/", Recipes.as_view(), name="recipes"),
-    path("<slug:pk>/", RecipeDetail.as_view(), name="recipe_detail"),
+    path("<slug:pk>/", RecipeDetail.as_view(), name="recipe_detail")
 ]
 
